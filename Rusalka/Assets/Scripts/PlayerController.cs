65bd0b20da28f01dfdafd5042485d7faa8cc3294
lilytengoku <ajawadiwar2@gmail.com>
2024-07-22 14:47:21 +0200

    Walk, Jump, Float
    Preliminary Walking, Jumping, and Floating

---
+++ Rusalka/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float MovementSpeed;
+    [SerializeField] private float UpGravityForce;
+    [SerializeField] private float DownGravityForce;
+    [SerializeField] private float FloatGravityForce;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float ReleaseSpeed;
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    private bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        velocity = new Vector2(0, 0);
+        grounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.x = Input.GetAxisRaw("Horizontal") * MovementSpeed;
+        if (!grounded)
+        {
+            float currGrav = DownGravityForce;
+            if (velocity.y > 0)
+            {
+                currGrav = UpGravityForce;
+            }
+            else if (Input.GetKey(KeyCode.Space)) {
+                currGrav = FloatGravityForce;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = 0;
+            }
+            velocity.y -= currGrav;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = JumpForce;
+            }
+            else if(velocity.y < 0){
+                velocity.y = 0;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && velocity.y > 0) {
+            velocity.y = ReleaseSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        grounded = Physics2D.OverlapCircle((Vector2)transform.position + Vector2.down * .5f, .1f, LayerMask.GetMask("Floor"));
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = Physics2D.OverlapCircle((Vector2)transform.position + Vector2.down * .5f, .1f, LayerMask.GetMask("Floor"));
+    }
+}
