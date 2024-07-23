cdcba8c56b81c60be2212087739274d410df9d6e
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2024-07-23 15:49:18 +0200

    main menu fix

--- Rusalka/Assets/Scripts/NavigatableMenuController.cs
+++ Rusalka/Assets/Scripts/NavigatableMenuController.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 public class NavigatableMenuController : Singleton<NavigatableMenuController>
 {
     private List<NavigatableMenuButton> activeButtons;
-    private const float inputDelayMaxTime = 0.1f;
+    private const float inputDelayMaxTime = 0.25f;
     private float inputDelayTimer;
     private int selectionId;
 
