5f43ed209a2c6fa09ad4eeb35b4af03711e150f5
JesterArc <125870978+JesterArc@users.noreply.github.com>
2024-07-23 16:18:54 +0200

    fixing syntax

--- Rusalka/Assets/Scripts/CameraScripts/ChangeCameraMode.cs
+++ Rusalka/Assets/Scripts/CameraScripts/ChangeCameraMode.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class ChangeCameraMode : MonoBehaviour
 {
-    [SerializeField] private CameraOperator.CameraMode Mode;
+    [SerializeField] private CameraOperator.CameraMode mode;
+    [SerializeField] private float newSize;
+    private float originalValue;
     public void OnTriggerEnter2D(Collider2D collider) {
         if(collider.CompareTag("Player") && CameraOperator.Instance != null){
-            CameraOperator.Instance.Set_CameraMode(Mode);
+            CameraOperator.Instance.SetCameraMode(mode);
+            switch(mode){
+                case CameraOperator.CameraMode.Normal:
+                    originalValue = CameraOperator.Instance.GetNormalSize();
+                    CameraOperator.Instance.SetNormalSize(newSize);
+                    break;
+                case CameraOperator.CameraMode.VistaPoint:
+                    originalValue = CameraOperator.Instance.GetVistaSize();
+                    CameraOperator.Instance.SetVistaSize(newSize);
+                    break;
+                default:
+                    break;
+            }
         }
     }
     public void OnTriggerExit2D(Collider2D collider) {
         if(collider.CompareTag("Player") && CameraOperator.Instance != null){
-            CameraOperator.Instance.Set_CameraMode(CameraOperator.CameraMode.Normal);
+            switch(mode){
+                case CameraOperator.CameraMode.Normal:
+                    CameraOperator.Instance.SetNormalSize(originalValue);
+                    break;
+                case CameraOperator.CameraMode.VistaPoint:
+                    CameraOperator.Instance.SetVistaSize(originalValue);
+                    break;
+                default:
+                    break;
+            }
+            CameraOperator.Instance.SetCameraMode(CameraOperator.CameraMode.Normal);
         }
+
     }    
 }
