5afc70528661d70437c05b729754b98c29e885f7
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2024-07-25 11:36:19 +0200

    slope fix 3

--- Rusalka/Assets/Scripts/PlayerController.cs
+++ Rusalka/Assets/Scripts/PlayerController.cs
@@ -251,7 +251,8 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - collide.bounds.extents.x + .01f, transform.position.y - collide.bounds.extents.y), new Vector2(transform.position.x + collide.bounds.extents.x - .01f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
+        grounded = Physics2D.OverlapArea(new Vector2(transform.position.x + .001f, transform.position.y - collide.bounds.extents.y), 
+            new Vector2(transform.position.x - .001f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
         if (grounded)
         {
             inGrapple = false;
@@ -260,7 +261,8 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - collide.bounds.extents.x + .01f, transform.position.y - collide.bounds.extents.y), new Vector2(transform.position.x + collide.bounds.extents.x - .01f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
+        grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - .001f, transform.position.y - collide.bounds.extents.y), 
+            new Vector2(transform.position.x - .001f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
