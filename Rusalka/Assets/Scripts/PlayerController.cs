75617d3f94c4cdf3ea746157c2ab17e53e1ebca7
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2024-07-25 11:15:24 +0200

    slope fix 1 / 199999999999999e23402384020304923

--- Rusalka/Assets/Scripts/PlayerController.cs
+++ Rusalka/Assets/Scripts/PlayerController.cs
@@ -260,11 +260,7 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
-        {
-            grounded = false;
-        }
-        //grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - collide.bounds.extents.x + .01f, transform.position.y - collide.bounds.extents.y), new Vector2(transform.position.x + collide.bounds.extents.x - .01f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
+        grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - collide.bounds.extents.x + .01f, transform.position.y - collide.bounds.extents.y), new Vector2(transform.position.x + collide.bounds.extents.x - .01f, transform.position.y - collide.bounds.extents.y - .01f), LayerMask.GetMask("Floor"));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
