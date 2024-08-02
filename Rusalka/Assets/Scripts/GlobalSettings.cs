09ed482dbd87d58ca26bb0e6230cee46a2c67942
Mark <125870978+JesterArc@users.noreply.github.com>
2024-08-02 16:33:07 +0200

    Pause Menu Implemented

--- Rusalka/Assets/Scripts/GlobalSettings.cs
+++ Rusalka/Assets/Scripts/GlobalSettings.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public enum Language
 {
     English, Polish, German, French, Spanish
@@ -8,4 +10,13 @@ public enum Language
 public class GlobalSettings : Singleton<GlobalSettings>
 {
     public static Language GlobalLanguage = Language.English;
+    public static float MasterVolume = 1;
+    public static float SFXVolume = 1;
+    public static float MusicVolume = 1;
+    public void changeMasterVolume(Slider slider) { MasterVolume = slider.value; }
+    public void changeSFXVolume(Slider slider) { SFXVolume = slider.value; }
+    public void changeMusicVolume(Slider slider) { MusicVolume = slider.value; }
+    public float getMasterVolume() { return  MasterVolume; }
+    public float getSFXVolume() {  return SFXVolume; }
+    public float getMusicVolume() {  return MusicVolume; }
 }
