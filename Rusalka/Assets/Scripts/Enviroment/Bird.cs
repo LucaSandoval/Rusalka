9401547d535924e815e8024099d5e6f20c156d00
KevinMcGrath1 <mcgrath.k@northeastern.edu>
2024-08-05 15:28:06 +0200

    Birds

--- Rusalka/Assets/Scripts/Enviroment/Bird.cs
+++ Rusalka/Assets/Scripts/Enviroment/Bird.cs
@@ -1,12 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
     [SerializeField] private float movingSpeed = 0.1f;
-    
+    [SerializeField] private float TravelDistance;
+    private Vector3 StartingPos;
+
+
+    private void Start()
+    {
+        Vector3 StartingPos = transform.position;
+    }
+
     void FixedUpdate()
     {
-        Vector3 newPosition = transform.position + Vector3.right * movingSpeed;
-        transform.position = newPosition;
+            Vector3 newPosition = transform.position + Vector3.right * movingSpeed;
+            transform.position = newPosition;
+       
+        
+
+        if(Vector3.Distance(transform.position, StartingPos) > TravelDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 }
