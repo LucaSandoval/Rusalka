4efa293e0e634a55abaa5c8c93d6216ec4796a97
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2024-08-06 15:11:40 +0200

    fixed build issue

--- Rusalka/Assets/Scripts/Player Interaction/Interactible.cs
+++ Rusalka/Assets/Scripts/Player Interaction/Interactible.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
 public class Interactible : MonoBehaviour
