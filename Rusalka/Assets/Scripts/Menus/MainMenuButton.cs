35243324e7b571c129202f3b213af5dcf92f19b9
Mark <125870978+JesterArc@users.noreply.github.com>
2024-08-06 10:37:14 +0200

    Sound buttons are now adjustable with controller

--- Rusalka/Assets/Scripts/Menus/MainMenuButton.cs
+++ Rusalka/Assets/Scripts/Menus/MainMenuButton.cs
@@ -5,12 +5,21 @@ using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 /// <summary>
 /// A button on the main menu.
 /// </summary>
 public class MainMenuButton : NavigatableMenuButton
 {
     public Image bgImage;
+    private bool isSelected;
+    private Slider slider;
+    private const float inputDelayMaxTime = 0.15f;
+    private float inputDelayTimer;
+    public void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
     public override void Choose()
     {
         Debug.Log("Chose Button!");
@@ -25,6 +34,15 @@ public class MainMenuButton : NavigatableMenuButton
                 break;
             case "SettingsButton":
                 FindObjectOfType<MainMenuController>().ChangeActiveButtons(Submenu.Settings);
+                break;
+            case "MasterVolume":
+                slider.value = (GlobalSettings.Instance.getMasterVolume() == 0 ? 1f : 0f); 
+                break;
+            case "SFXVolume":
+                slider.value = (GlobalSettings.Instance.getSFXVolume() == 0 ? 1f : 0f);
+                break;
+            case "MusicVolume":
+                slider.value = (GlobalSettings.Instance.getMusicVolume() == 0 ? 1f : 0f);
                 break;
             case "BackButton":
                 FindObjectOfType<MainMenuController>().ChangeActiveButtons(Submenu.Menu);
@@ -44,10 +62,49 @@ public class MainMenuButton : NavigatableMenuButton
     public override void Deselect()
     {
         bgImage.CrossFadeColor(Color.white, 0f, false, true);
+        isSelected = false;
     }
 
     public override void Select()
     {
         bgImage.CrossFadeColor(new Color(1, 0.77f, 0.1f, 1f), 0.2f, false, true);
+        isSelected = true;
+    }
+    // Delays the player input. 
+    private void DelayInput()
+    {
+        inputDelayTimer = inputDelayMaxTime;
+    }
+
+    // Checks if the input is currently being delayed.
+    private bool IsInputDelayed()
+    {
+        return inputDelayTimer > 0;
+    }
+    public void Update()
+    {
+        if ((name != "MasterVolume" && name != "SFXVolume" && name != "MusicVolume") || !isSelected) return;
+        if (Input.GetAxisRaw("Horizontal") != 0 && !IsInputDelayed())
+            {
+            switch (name)
+            {
+                case "MasterVolume":
+                    slider.value = GlobalSettings.Instance.getMasterVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeMasterVolume(slider);
+                    DelayInput();
+                    break;
+                case "SFXVolume":
+                    slider.value = GlobalSettings.Instance.getSFXVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeSFXVolume(slider);
+                    DelayInput();
+                    break;
+                case "MusicVolume":
+                    slider.value = GlobalSettings.Instance.getMusicVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeMusicVolume(slider);
+                    DelayInput();
+                    break;
+            }
+            }
+        else if (IsInputDelayed()) inputDelayTimer -= Time.deltaTime;
     }
 }
--- Rusalka/Assets/Scripts/Menus/PauseMenuButton.cs
+++ Rusalka/Assets/Scripts/Menus/PauseMenuButton.cs
@@ -7,6 +7,14 @@ using UnityEngine.UI;
 public class PauseMenuButton : NavigatableMenuButton
 {
     public Image bgImage;
+    private bool isSelected;
+    private Slider slider;
+    private const float inputDelayMaxTime = 0.15f;
+    private float inputDelayTimer;
+    public void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
     public override void Choose()
     {
         Debug.Log("Chose Button!");
@@ -17,6 +25,15 @@ public class PauseMenuButton : NavigatableMenuButton
                 break;
             case "Settings":
                 PauseController.Instance.ChangeActiveButtons(pauseState.Settings);
+                break;
+            case "MasterVolume":
+                slider.value = (GlobalSettings.Instance.getMasterVolume() == 0 ? 1f : 0f); 
+                break;
+            case "SFXVolume":
+                slider.value = (GlobalSettings.Instance.getSFXVolume() == 0 ? 1f : 0f);
+                break;
+            case "MusicVolume":
+                slider.value = (GlobalSettings.Instance.getMusicVolume() == 0 ? 1f : 0f);
                 break;
             case "Language":
                 GlobalSettings.GlobalLanguage = (Language)(((int)GlobalSettings.GlobalLanguage + 1) % 6);
@@ -34,10 +51,50 @@ public class PauseMenuButton : NavigatableMenuButton
     public override void Deselect()
     {
         bgImage.CrossFadeColor(Color.white, 0f, false, true);
+        isSelected = false;
     }
 
     public override void Select()
     {
         bgImage.CrossFadeColor(new Color(1, 0.77f, 0.1f, 1f), 0.2f, false, true);
+        isSelected = true;
+    }
+
+    // Delays the player input. 
+    private void DelayInput()
+    {
+        inputDelayTimer = inputDelayMaxTime;
+    }
+
+    // Checks if the input is currently being delayed.
+    private bool IsInputDelayed()
+    {
+        return inputDelayTimer > 0;
+    }
+    public void Update()
+    {
+        if ((name != "MasterVolume" && name != "SFXVolume" && name != "MusicVolume") || !isSelected) return;
+        if (Input.GetAxisRaw("Horizontal") != 0 && !IsInputDelayed())
+        {
+            switch (name)
+            {
+                case "MasterVolume":
+                    slider.value = GlobalSettings.Instance.getMasterVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeMasterVolume(slider);
+                    DelayInput();
+                    break;
+                case "SFXVolume":
+                    slider.value = GlobalSettings.Instance.getSFXVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeSFXVolume(slider);
+                    DelayInput();
+                    break;
+                case "MusicVolume":
+                    slider.value = GlobalSettings.Instance.getMusicVolume() + 0.1f * (Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1);
+                    GlobalSettings.Instance.changeMusicVolume(slider);
+                    DelayInput();
+                    break;
+            }
+        }
+        else if (IsInputDelayed()) inputDelayTimer -= Time.deltaTime;
     }
 }
