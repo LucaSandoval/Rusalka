aa5a2de0e9b3ee6e0959b122880f0e07f85a767e
koliia <koliiaihk@gmail.com>
2024-08-06 16:36:38 +0200

    [Scene][TagManager] Fish, FirstTower and Stelagmite castle added to the Underwater V5

--- Rusalka/Assets/Scripts/UnderWater/Environment/FlyFish.cs
+++ Rusalka/Assets/Scripts/UnderWater/Environment/FlyFish.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +6,47 @@ public class FlyFish : MonoBehaviour {
     [SerializeField] private List<Transform> points;
     [SerializeField] private Transform fish;
 
+    public delegate void ColliderListener();
+    public static ColliderListener OnCollisionFired;
+
     private float interpolateAmount;
     [SerializeField] private float playingSpeed = 0.4f;
 
+    [SerializeField] private Animator anim;
+
+    private bool hasFired = false;
+    
+    [Header("Timer")]
+    [SerializeField] float targetTime = 2.0f;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        
+        OnCollisionFired += Fired;
+    }
+
+    private void Fired()
+    {
+        hasFired = true;
+    }
+
     private void Update() {
-        interpolateAmount = (interpolateAmount + Time.deltaTime * playingSpeed) % 1f;
+        if(!hasFired) return;
+        
+        interpolateAmount = (interpolateAmount + Time.deltaTime * playingSpeed);
+        
+        if (interpolateAmount % 1f == 0f)
+        {
+            enabled = false;
+            Destroy(this);
+        }
+        
+        targetTime -= Time.deltaTime;
+        if(targetTime <= 0.0f)
+        {
+            anim.SetTrigger("IsFlying");
+        }
 
         if (points.Count >= 2) {
             Vector3 previousPosition = fish.position;
