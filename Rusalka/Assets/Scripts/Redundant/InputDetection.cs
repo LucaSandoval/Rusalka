833e12be050cc037c68ef218f8c6b4720be00bc3
Mark <125870978+JesterArc@users.noreply.github.com>
2024-08-09 11:09:00 +0200

    Now actually checks for controller input

--- Rusalka/Assets/Scripts/Redundant/InputDetection.cs
+++ Rusalka/Assets/Scripts/Redundant/InputDetection.cs
@@ -12,24 +12,35 @@ public class InputDetection : MonoBehaviour
 
     void DetectInputDevice()
     {
-        if (Input.GetKeyDown(KeyCode.A) || 
-            Input.GetKeyDown(KeyCode.D) ||
-            Input.GetKeyDown(KeyCode.W) ||
-            Input.GetKeyDown(KeyCode.S) ||
-            Input.GetKeyDown(KeyCode.RightArrow) ||
-            Input.GetKeyDown(KeyCode.LeftArrow) ||
-            Input.GetKeyDown(KeyCode.UpArrow) ||
-            Input.GetKeyDown(KeyCode.DownArrow) ||
-            Input.GetKeyDown(KeyCode.Space) ||
-            Input.GetKeyDown(KeyCode.Z)||
-            Input.GetKeyDown(KeyCode.KeypadEnter)||
-            Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            isUsingKeyboard = true;
+        if 
+        (
+            Input.GetKeyDown(KeyCode.JoystickButton0) ||
+            Input.GetKeyDown(KeyCode.JoystickButton1) ||
+            Input.GetKeyDown(KeyCode.JoystickButton2) ||
+            Input.GetKeyDown(KeyCode.JoystickButton3) ||
+            Input.GetKeyDown(KeyCode.JoystickButton4) ||
+            Input.GetKeyDown(KeyCode.JoystickButton5) ||
+            Input.GetKeyDown(KeyCode.JoystickButton6) ||
+            Input.GetKeyDown(KeyCode.JoystickButton7) ||
+            Input.GetKeyDown(KeyCode.JoystickButton8) ||
+            Input.GetKeyDown(KeyCode.JoystickButton9) ||
+            Input.GetKeyDown(KeyCode.JoystickButton10) ||
+            Input.GetKeyDown(KeyCode.JoystickButton11) ||
+            Input.GetKeyDown(KeyCode.JoystickButton12) ||
+            Input.GetKeyDown(KeyCode.JoystickButton13) ||
+            Input.GetKeyDown(KeyCode.JoystickButton14) ||
+            Input.GetKeyDown(KeyCode.JoystickButton15) ||
+            Input.GetKeyDown(KeyCode.JoystickButton16) ||
+            Input.GetKeyDown(KeyCode.JoystickButton17) ||
+            Input.GetKeyDown(KeyCode.JoystickButton18) ||
+            Input.GetKeyDown(KeyCode.JoystickButton19)
+        )
+        { 
+            isUsingKeyboard = false; 
         }
         else if (Input.anyKeyDown)
         {
-            isUsingKeyboard = false;
+            isUsingKeyboard = true;
         }
     }
 
