e209d1a9d4d8ea1c65b17241d6a3291ad3997203
KevinMcGrath1 <mcgrath.k@northeastern.edu>
2024-08-12 14:40:52 +0200

    Level One BerkayEdits

---
+++ Rusalka/Assets/SpawnDelay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(ShowAndHide(7.0f));
+        SpriteRenderer spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+}
