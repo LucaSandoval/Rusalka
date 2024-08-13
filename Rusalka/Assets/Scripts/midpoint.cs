fef0e8195e85cf9df4a73b9387b7fb8955fe87f5
koliia <koliiaihk@gmail.com>
2024-08-13 12:57:06 +0200

    changes

--- Rusalka/Assets/Scripts/midpoint.cs
+++ Rusalka/Assets/Scripts/midpoint.cs
@@ -3,30 +3,20 @@ using UnityEngine;
 
 public class midpoint : MonoBehaviour
 {
-    public Transform player;
+    [SerializeField] private float convergenceSpeed = 0.1f;
 
-    private float initialOffsetX;
-
-    //private float distanceTraveled;
-
+    [SerializeField] private Vector3 plusTargetLocation;
+    
     private bool hasFired = false;
-
-    void Start()
-    {
-        initialOffsetX = transform.position.x - player.position.x;
-    }
 
     void Update()
     {
-        if(!hasFired) return;
-        float newXPosition = player.position.x + initialOffsetX / 9;
-        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
-        //distanceTraveled += player.position.x;
+        if(!hasFired || Input.GetAxisRaw("Horizontal") == 0) return;
+        transform.position = Vector3.Lerp(transform.position, transform.position + plusTargetLocation, convergenceSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("herre");
         hasFired = true;
     }
 }
